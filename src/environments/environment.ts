147aafdbf52b708e5c8999b323056510ee599b2e
Margarita Delikatnaya <rita.delikatnaya@mail.ru>
2022-04-16 17:22:02 +0300

    fix server js script

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: {
+    apiBaseUri: 'https://ezha-by-app.herokuapp.com/',
+  },
   apiBaseUri: 'https://ezha-by-app.herokuapp.com/',
 };
 
